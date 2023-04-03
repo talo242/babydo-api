7690adcac415fabbfed6c26f0abee1527ff3d371
Ricardo Velasco <talo242@gmail.com>
2023-04-02 19:01:13 -0600

    feature(user-module): add /me endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { PrismaModule } from './prisma/prisma.module';
     }),
     AuthModule,
     PrismaModule,
+    UserModule,
   ],
 })
 export class AppModule {}
